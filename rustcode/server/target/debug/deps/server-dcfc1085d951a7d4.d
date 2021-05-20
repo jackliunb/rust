b3de8dedf0072175e0a8d977c91dad64be2d97c2
jackliunb <80453760+jackliunb@users.noreply.github.com>
2021-05-20 16:20:24 +0800

    Add files via upload

---
+++ rustcode/server/target/debug/deps/server-dcfc1085d951a7d4.d
@@ -0,0 +1,5 @@
+/home/lzh1/rustcode/server/target/debug/deps/server-dcfc1085d951a7d4: src/main.rs
+
+/home/lzh1/rustcode/server/target/debug/deps/server-dcfc1085d951a7d4.d: src/main.rs
+
+src/main.rs:
---
+++ rustcode/server/target/debug/server.d
@@ -0,0 +1,1 @@
+/home/lzh1/rustcode/server/target/debug/server: /home/lzh1/rustcode/server/src/main.rs
